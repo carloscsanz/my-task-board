b58c48b1819eea786bf3952e9088ce0d68db3693
Carlos Contreras Sanz <contrerassanz.carlos@gmail.com>
2024-09-24 23:34:44 +0200

    Create get tasks endpoint

--- src/main/kotlin/com/carloscsanz/mytaskboard/task/api/GetTasksEndpoint.kt
+++ src/main/kotlin/com/carloscsanz/mytaskboard/task/api/GetTasksEndpoint.kt
@@ -2,17 +2,21 @@ package com.carloscsanz.mytaskboard.task.api
 
 import com.carloscsanz.mytaskboard.task.api.dto.Task
 import com.carloscsanz.mytaskboard.task.api.dto.TaskStatus
+import org.springframework.http.HttpStatus
+import org.springframework.http.MediaType
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.ResponseStatus
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
 class GetTasksEndpoint {
-    @GetMapping("/tasks")
+    @GetMapping("/tasks", produces = [MediaType.APPLICATION_JSON_VALUE])
+    @ResponseStatus(HttpStatus.OK)
     fun getTasks(): Collection<Task> {
         return listOf(
             Task("Task in Progress", "", TaskStatus.IN_PROGRESS),
             Task("Task Completed", "", TaskStatus.DONE),
-            Task("Task Won’t Do", "", TaskStatus.WONT_DO),
+            Task("Task Won't Do", "", TaskStatus.WONT_DO),
             Task("Task To Do", "Work on a Challenge on devChallenges.io, learn TypeScript.", TaskStatus.TO_DO),
         )
     }
---
+++ src/test/kotlin/com/carloscsanz/mytaskboard/task/api/GetTasksEndpointTest.kt
@@ -0,0 +1,49 @@
+package com.carloscsanz.mytaskboard.task.api
+
+import org.junit.jupiter.api.Test
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.web.servlet.MockMvc
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers.content
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers.status
+
+@SpringBootTest
+@AutoConfigureMockMvc
+class GetTasksEndpointTest(
+    @Autowired private val mockMvc: MockMvc
+) {
+    @Test
+    fun `should retrieve all the the tasks`() {
+        val expected = """
+            [
+                {
+                    "name": "Task in Progress",
+                    "description": "",
+                    "status": "IN_PROGRESS"
+                },
+                {
+                    "name": "Task Completed",
+                    "description": "",
+                    "status": "DONE"
+                },
+                {
+                    "name": "Task Won't Do",
+                    "description": "",
+                    "status": "WONT_DO"
+                },
+                {
+                    "name": "Task To Do",
+                    "description": "Work on a Challenge on devChallenges.io, learn TypeScript.",
+                    "status":"TO_DO"
+                }
+            ]
+        """
+
+        mockMvc
+            .perform(get("/tasks"))
+            .andExpect(status().isOk())
+            .andExpect(content().json(expected, true));
+    }
+}
